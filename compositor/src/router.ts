838e0d56fdc5240b429991f20ecf8db56c8aa1ae
cr9169 <bar.gabay.home@gmail.com>
2024-10-16 23:05:19 +0300

    feat: added createProxyMiddleware to compositor central router

--- compositor/src/router.ts
+++ compositor/src/router.ts
@@ -1,9 +1,23 @@
 import { Router } from "express";
 import { CompositorRouter } from "./compositor/router";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const appRouter = Router();
 
-appRouter.use("/", CompositorRouter);
+appRouter.use(
+  "/users",
+  createProxyMiddleware({
+    target: "http://localhost:4000",
+    changeOrigin: true,
+  })
+);
+
+appRouter.use(
+  "/groups",
+  createProxyMiddleware({ target: "http://localhost:5000", changeOrigin: true })
+);
+
+appRouter.use("/compositor", CompositorRouter);
 
 appRouter.use("/isAlive", (_req, res) => {
   res.status(200).json("UP!");
